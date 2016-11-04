9a0f1de039049ee0fa115a1486f5fc872446de63
sbociot <stevebocicot@icloud.com>
2016-11-04 12:45:14 -0400

    “baknajkiausdb”

---
+++ script/main.js
@@ -0,0 +1,9 @@
+"use strict";
+var rep = 20;
+var rep_utilisateur = prompt('Que vaut 10+10');
+if (rep_utilisateur == null) {
+	document.write('<p>Au revioir</p>');
+}else{
+	rep_utilisateur = prompt('Faux Que vaut 10+10');
+}
+
